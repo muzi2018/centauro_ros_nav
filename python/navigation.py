3c90ee6cb97b79f981d3568f05fa00ed7ac2f608
Weijie Wang <duangsir518@163.com>
2025-03-24 12:42:51 +0100

    publish tf frame

--- python/navigation.py
+++ python/navigation.py
@@ -11,6 +11,7 @@ from std_msgs.msg import String
 import tf2_ros
 import tf2_geometry_msgs
 from geometry_msgs.msg import PointStamped
+import geometry_msgs.msg
 
 # map info
 # resolution: 0.05
@@ -23,6 +24,35 @@ from geometry_msgs.msg import PointStamped
 # Chair 2: (0.75, 1.12, 8.4) -> (8.509858507416597, 0.21297738285356893, -0.7844677892095936)
 # Chair 3: (-1.33, 0.83, 3.61) -> (3.512881381696345, 1.7570635315974463, -0.8032891220488297)
 
+'''
+tf2_ros.StaticTransformBroadcaster
+'''
+
+class ChairTFBroadcaster:
+    def __init__(self):
+        self.br = tf2_ros.TransformBroadcaster()
+    
+    def publish_chair_tf(self, chair_name, x, y, z, yaw):
+        t = geometry_msgs.msg.TransformStamped()
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = "map"  # The chair should be aligned with the map
+        t.child_frame_id = chair_name  # e.g., "chair_1"
+        
+        t.transform.translation.x = x
+        t.transform.translation.y = y
+        t.transform.translation.z = z
+
+        quat = tf.quaternion_from_euler(0, 0, yaw)
+        t.transform.rotation.x = quat[0]
+        t.transform.rotation.y = quat[1]
+        t.transform.rotation.z = quat[2]
+        t.transform.rotation.w = quat[3]
+
+        self.br.sendTransform(t)
+
+        
+        
+chair_tf_broadcaster = ChairTFBroadcaster()
 Chairs_dict = {"chair_1": {"position":(0.0, 0.0, 0.0), "record": False}}
 update_flag = True
 
@@ -87,8 +117,11 @@ def send_waypoints():
                 Chairs_dict["chair_1"]["position"] = transformed_pos
                 Chairs_dict["chair_1"]["record"] = True
                 pos_o = obj_dict["chair_1"]["position"]
+                x, y, z = transformed_pos
+                yaw = 0
+                chair_tf_broadcaster.publish_chair_tf("chair_1", x, y, z, yaw)
                 print(f"Original Chair 1 pos -> {pos_o}")
-                print(f"Chair 1 transformed_pos -> {transformed_pos}")
+                print(f"Chair 1 transformed_pos -> {transformed_pos}") # Chair 1 transformed_pos -> (3.492636803450569, -0.6283296429808908, -0.9809153977970663)
 
             if cnt == 0:
                 waypoints = [(Chairs_dict["chair_1"]["position"][0], Chairs_dict["chair_1"]["position"][1]-1.3, 0.45)]
@@ -132,3 +165,5 @@ if __name__ == "__main__":
         send_waypoints()
     except rospy.ROSInterruptException:
         rospy.logerr("ROS interrupted. Exiting...")
+
+
